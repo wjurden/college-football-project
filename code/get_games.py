4d4230323bd32c826ba9a5fca9dc8a88e5692f42
wesjurden <wjurden@gmail.com>
2022-12-14 20:35:49 -0800

    Update get_games.py
    Working to loop through all the values in each game to work towards creating a data frame

--- code/get_games.py
+++ code/get_games.py
@@ -1,3 +1,5 @@
+#Install cfbd using pip install then import
+
 import cfbd
 import creds
 import os
@@ -19,6 +21,44 @@ games
 i = 0 
 
 for game in games:
-    print(games[i].home_team)
+    attendance = games[i].attendance
+    away_conference = games[i].away_conference
+    away_division = games[i].away_division
+    away_id = games[i].away_id
+    away_line_scores = games[i].away_line_scores
+    away_points = games[i].away_points
+    away_post_win_prob = games[i].away_post_win_prob
+    away_postgame_elo = games[i].away_postgame_elo
+    away_pregame_elo = games[i].away_pregame_elo
+    away_team = games[i].away_team
+    completed = games[i].completed
+    conference_game = games[i].conference_game
+    excitement_index = games[i].excitement_index
+    highlights = games[i].highlights
+    home_conference = games[i].home_conference
+    home_division = games[i].home_division
+    home_id = games[i].home_id
+    home_line_scores = games[i].home_line_scores
+    home_points = games[i].home_points
+    home_post_win_prob = games[i].home_post_win_prob
+    home_postgame_elo = games[i].home_postgame_elo
+    home_pregame_elo = games[i].home_pregame_elo
+    home_team = games[i].home_team
+    id = games[i].id
+    neutral_site = games[i].neutral_site
+    notes = games[i].notes
+    season = games[i].season
+    season_type = games[i].season_type
+    start_date = games[i].start_date
+    start_time_tbd = games[i].start_time_tbd
+    venue = games[i].venue
+    venue_id = games[i].venue_id
+    week = games[i].week
+    games_ls = [attendance, away_conference, away_division, away_id, away_line_scores, away_points, away_post_win_prob, 
+                away_postgame_elo, away_pregame_elo, away_team, completed, conference_game, excitement_index, highlights, 
+                home_conference, home_division, home_id, home_line_scores, home_points, home_post_win_prob, home_postgame_elo, 
+                home_pregame_elo, home_team, id, neutral_site, notes, season, season_type, start_date, start_time_tbd, venue, 
+                venue_id, week]
     i +=1
+    print(games_ls)
 
