e3ccb094fb3f79be922bee2fca10cf2a2daad142
wesjurden <wjurden@gmail.com>
2022-12-15 19:02:57 -0800

    Update get_games.py
    Updating file to pull all games of 2022

--- code/get_games.py
+++ code/get_games.py
@@ -1,25 +1,42 @@
 #Install cfbd using pip install then import
 
+#Importing packages
 import cfbd
 import creds
 import os
+import pandas as pd
 
 #Change working directory to 'code' directory
 os.chdir('/Users/wesjurden/Documents/GitHub/Personal/college-football-project/code')
 
+#===============================
 # Configuring API
+#===============================
 configuration = cfbd.Configuration()
 configuration.api_key['Authorization'] = f'{creds.api_key}'
 configuration.api_key_prefix['Authorization'] = 'Bearer'
 
-#Creating an instance of the API for all games in 2022
+#===============================
+# Creating an instance of the API for all games in 2022
+#===============================
 api_instance = cfbd.GamesApi(cfbd.ApiClient(configuration))
 games = api_instance.get_games(year=2022)
-games
 
-#Beginning to look through data
+#Creating an empty df that will hold the values of the game data
+df = pd.DataFrame(columns= ['attendance', 'away_conference', 'away_division', 'away_id', 'away_line_scores', 'away_points', 'away_post_win_prob', 
+                'away_postgame_elo', 'away_pregame_elo', 'away_team', 'completed', 'conference_game', 'excitement_index', 'highlights', 
+                'home_conference', 'home_division', 'home_id', 'home_line_scores', 'home_points', 'home_post_win_prob', 'home_postgame_elo', 
+                'home_pregame_elo', 'home_team', 'id', 'neutral_site', 'notes', 'season', 'season_type', 'start_date', 'start_time_tbd', 'venue', 
+                'venue_id', 'week'])
+
+#===============================
+# Beginning to look through data
+#===============================
+
+#Counter
 i = 0 
 
+#Creating a dataframe of the data
 for game in games:
     attendance = games[i].attendance
     away_conference = games[i].away_conference
@@ -54,11 +71,21 @@ for game in games:
     venue = games[i].venue
     venue_id = games[i].venue_id
     week = games[i].week
+
+    #Storing all values in a list
     games_ls = [attendance, away_conference, away_division, away_id, away_line_scores, away_points, away_post_win_prob, 
                 away_postgame_elo, away_pregame_elo, away_team, completed, conference_game, excitement_index, highlights, 
                 home_conference, home_division, home_id, home_line_scores, home_points, home_post_win_prob, home_postgame_elo, 
                 home_pregame_elo, home_team, id, neutral_site, notes, season, season_type, start_date, start_time_tbd, venue, 
                 venue_id, week]
-    i +=1
+
+    #Adding values to dataframe
+    df.loc[i] = games_ls
+
+    i += 1
     print(games_ls)
+
+#Changing directory and then saving file
+os.chdir('/Users/wesjurden/Documents/GitHub/Personal/college-football-project/data')
+df.to_csv('raw_game_data_2022.csv')
 
