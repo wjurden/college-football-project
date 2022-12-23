ec303cc1c5a30b5234998e429a67639a138c1cbe
wesjurden <wjurden@gmail.com>
2022-12-23 12:21:08 -0700

    Create requests.py
    Creating an alternative way to pull the data using a more traditional method to pull the data vs. a custom CFDB package

---
+++ code/extract/requests.py
@@ -0,0 +1,27 @@
+#===============================
+# Using REQUESTS to pull the data instead of custon CFDB package
+#===============================
+
+#Importing packages
+import creds
+import os
+import requests
+import json
+
+#Change working directory to 'code' directory so creds can be passed
+os.chdir('/Users/wesjurden/Documents/GitHub/Personal/college-football-project/code')
+
+#===============================
+# Configuring API
+#===============================
+endpoint = 'https://api.collegefootballdata.com/games?year=2022'
+headers = {"Authorization": f"Bearer {creds.api_key}"}
+
+#===============================
+# Creating an instance of the API for all games in 2022
+#===============================
+response = requests.get(endpoint,headers=headers)
+json_response = response.json()
+
+#Pretty print
+print(json.dumps(json_response, indent=2))
