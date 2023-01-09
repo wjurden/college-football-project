345300cc0f426236b14f43bf5b68e34d912589f4
wesjurden <wjurden@gmail.com>
2023-01-09 05:12:35 -0800

    Update get_plays.py
    Adding loop to file to extract the data

--- code/extract/get_plays.py
+++ code/extract/get_plays.py
@@ -4,15 +4,15 @@
 #===============================
 
 #===============================
-# Using REQUESTS to pull the data instead of custon CFDB package
+# Using REQUESTS to pull the data. This script pulls data for 2022 regular season data only. 
 #===============================
 
 # Importing packages
 import creds
 import os
 import requests
-import json
 import pandas as pd
+from datetime import datetime 
 
 # Change working directory to 'code' directory so creds can be passed
 os.chdir('/Users/wesjurden/Documents/GitHub/Personal/college-football-project/code')
@@ -26,26 +26,30 @@ os.chdir('/Users/wesjurden/Documents/GitHub/Personal/college-football-project/co
 # Creating a list of week numbers that will be looped through
 week = list(range(1,15))
 
-# Configuring the base endpoint and headers
-endpoint = f'https://api.collegefootballdata.com/plays?seasonType=regular&year=2022&week={w}' #Setting year to 2022, and season to regular
+# Configuring the api header
 headers = {"Authorization": f"Bearer {creds.api_key}"}
 
 #===============================
 # Pulling Data
 #===============================
-response = requests.get(endpoint,headers=headers)
-json_response = response.json()
+
 
 # Pretty print
-print(json.dumps(json_response, indent=2))
+# print(json.dumps(json_response, indent=2))
 
 #===============================
 # Creating List of Values
 #===============================
-
-# Need to write a for loop that loops through the weeks and adds them to a data frame
+start_time = datetime.now()
 
 for w in week:
+    
+    # Pulling data for each week
+    endpoint = f'https://api.collegefootballdata.com/plays?seasonType=regular&year=2022&week={w}' #Setting year to 2022, and season to regular
+    response = requests.get(endpoint,headers=headers)
+    json_response = response.json()
+
+    # Looping through data to add it to a dataframe
     if w == 1:
         # Pulling keys of first list object
         column_names = json_response[1].keys()
@@ -56,10 +60,19 @@ for w in week:
         # Adding games to dataframe
         for play in json_response:
             df.loc[len(df)] = play.values()
+        
+        df.tail()
     else:
         # Adding games to dataframe
         for play in json_response:
             df.loc[len(df)] = play.values()
+        
+        df.tail()
+
+# Printing how long the loop took to run
+end_time = datetime.now()
+final_time = end_time - start_time
+print(final_time)
 
 #===============================
 # Saving file
