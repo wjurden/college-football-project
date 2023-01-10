dbc3b6ffd3b95df749e0fdb02b32e3b3a9f0daec
wesjurden <wjurden@gmail.com>
2023-01-09 19:35:58 -0800

    Update get_plays.py
    Adding pretty print to see json

--- code/extract/get_plays.py
+++ code/extract/get_plays.py
@@ -13,6 +13,7 @@ import os
 import requests
 import pandas as pd
 from datetime import datetime 
+import json
 
 # Change working directory to 'code' directory so creds can be passed
 os.chdir('/Users/wesjurden/Documents/GitHub/Personal/college-football-project/code')
@@ -30,24 +31,17 @@ week = list(range(1,15))
 headers = {"Authorization": f"Bearer {creds.api_key}"}
 
 #===============================
-# Pulling Data
-#===============================
-
-
-# Pretty print
-# print(json.dumps(json_response, indent=2))
-
-#===============================
 # Creating List of Values
 #===============================
 start_time = datetime.now()
-
+ 
 for w in week:
     
     # Pulling data for each week
     endpoint = f'https://api.collegefootballdata.com/plays?seasonType=regular&year=2022&week={w}' #Setting year to 2022, and season to regular
     response = requests.get(endpoint,headers=headers)
     json_response = response.json()
+    print(json.dumps(json_response, indent=2))
 
     # Looping through data to add it to a dataframe
     if w == 1:
@@ -60,13 +54,11 @@ for w in week:
         # Adding games to dataframe
         for play in json_response:
             df.loc[len(df)] = play.values()
-        
         df.tail()
     else:
         # Adding games to dataframe
         for play in json_response:
             df.loc[len(df)] = play.values()
-        
         df.tail()
 
 # Printing how long the loop took to run
