0bc514d65cb5d9222d50efc29d0da792332e8ae2
wjurden <wjurden@gmail.com>
2023-01-25 05:11:05 -0800

    Update clean_player_game_stats.py
    Converting data types to strings

--- code/clean/clean_player_game_stats.py
+++ code/clean/clean_player_game_stats.py
@@ -1,1 +1,21 @@
-# Will need to remove duplicate values from this file
+#===============================
+# The purpose of this file is to clean raw_player_game_stats_data_2022.csv
+#===============================
+
+import os
+import pandas as pd
+
+# Reading in data
+os.chdir('/Users/wesjurden/Documents/GitHub/Personal/college-football-project/data/raw')
+df = pd.read_csv('raw_player_game_stats_data_2022.csv')
+
+# Checking column attributes
+df.columns
+df.info()
+df.iloc[0]
+
+# Converting data types
+df['game_id'] = df['game_id'].astype('str')
+df['athlete_id'] = df['athlete_id'].astype('str')
+
+df.dtypes
