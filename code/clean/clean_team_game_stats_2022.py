3cf9c14f4c33a7cb3ea39112edd9f53865f7dc09
wjurden <wjurden@gmail.com>
2023-02-12 09:06:49 -0800

    Update clean_team_game_stats_2022.py
    Cleaning values for team game stats. Still WIP

--- code/clean/clean_team_game_stats_2022.py
+++ code/clean/clean_team_game_stats_2022.py
@@ -10,4 +10,63 @@ df.columns
 df.info()
 df.iloc[0]
 
-# Work on editing data types for down effiencies and pass attempts
+#=================
+# Converting Data Types
+#=================
+
+# Converting completion attempts
+df[['completions', 'attempts']] = df['completionAttempts'].str.split('-', expand=True)
+
+# Convert data type
+df['attempts'] = df['attempts'].astype('Int64')
+df['completions'] = df['completions'].astype('Int64')
+
+# Create a completion percentage column
+df['completionPercentage'] = (df['completions'] / df['attempts']).astype('float64')
+
+#=================
+# Cleaning fourth down eff
+#=================
+# This returns an erorr - need to clean values first
+df[['fourth_success', 'fourth_attempts']] = df['fourthDownEff'].str.split('-', expand=True)
+
+# Need to clean up this column - has a few values where there are multiple dashes and a large number of 4th downs
+df['fourthDownEff'].unique()
+
+# Changing values of errors
+df[df['fourthDownEff'] == '0--1']
+df.at[2481,'fourthDownEff'] = '0-1'
+
+# Checking changes
+df['fourthDownEff'].unique()
+
+df[df['fourthDownEff'] == '1--1']
+df.at[2940,'fourthDownEff'] = '1-1'
+
+# Checking changes
+df['fourthDownEff'].unique()
+
+df[df['fourthDownEff'] == '1--3']
+df.at[2941,'fourthDownEff'] = '1-3'
+
+# Checking changes
+df['fourthDownEff'].unique()
+
+df[df['fourthDownEff'] == '3-55']
+df.at[3032,'fourthDownEff'] = '3-5'
+
+# This returns an erorr - need to clean values first
+df[['fourth_success', 'fourth_attempts']] = df['fourthDownEff'].str.split('-', expand=True)
+
+# Convert data type
+df['fourth_success'] = df['fourth_success'].astype('Int64')
+df['fourth_attempts'] = df['fourth_attempts'].astype('Int64')
+
+# Create a completion percentage column
+df['fourthEffpct'] = (df['fourth_success'] / df['fourth_attempts']).astype('float64')
+
+#=================
+# Cleaning Possession time
+#=================
+
+df.info()
