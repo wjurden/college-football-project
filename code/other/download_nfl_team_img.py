16541bb221ad1763a86e1d4c58bcd4431bceabe6
wjurden <wjurden@gmail.com>
2023-04-09 15:09:20 -0700

    Create download_nfl_team_img.py
    Script to download and save all nfl logos

---
+++ code/other/download_nfl_team_img.py
@@ -0,0 +1,34 @@
+# pip install requests
+# pip install os
+# pip install pandas
+# pip install pillow
+import requests
+from PIL import Image
+from io import BytesIO
+import os
+import pandas as pd
+ 
+
+# Change directory
+os.chdir('/Users/wesjurden/Documents/GitHub/Personal/college-football-project/data/raw') # Change '/Users/WJurde/Documents' to folder where your product images are saved
+ 
+# Reading in data
+df = pd.read_csv('raw_draft_teams.csv') # Change 'product_images.csv' to whatever your csv is called (this file needs to have no headers - the first row needs to be a url)
+df.head()
+ 
+i=0
+# Save files as pngs
+for link in df['logo']:
+   
+    # Get style code
+    file_name  = str.replace(df['displayName'][i],' ','_').lower()
+    print(file_name)
+
+    # Get request and image
+    response = requests.get(link)
+    img = Image.open(BytesIO(response.content))
+ 
+    # Save image
+    img.save(f'/Users/wesjurden/Documents/GitHub/Personal/college-football-project/assets/nfl_teams/{file_name}.png')
+
+    i += 1
