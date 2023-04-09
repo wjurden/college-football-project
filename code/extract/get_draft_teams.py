e17b2da8b289d634ed73548ec256b47deb4b05e6
wjurden <wjurden@gmail.com>
2023-04-09 15:11:11 -0700

    Create get_draft_teams.py
    Script to get all NFL teams from API

---
+++ code/extract/get_draft_teams.py
@@ -0,0 +1,43 @@
+#===============================
+# Using REQUESTS to pull the data
+#===============================
+
+# Importing packages
+import creds
+import os
+import requests
+import json
+import pandas as pd
+
+# Change working directory to 'code' directory so creds can be passed
+os.chdir('/Users/wesjurden/Documents/GitHub/Personal/college-football-project/code')
+
+#===============================
+# Configuring API
+#===============================
+endpoint = 'https://api.collegefootballdata.com/draft/teams' 
+headers = {"Authorization": f"Bearer {creds.api_key}"}
+
+#===============================
+# Pulling Data
+#===============================
+response = requests.get(endpoint,headers=headers)
+json_response = response.json()
+
+# Pretty print
+print(json.dumps(json_response, indent=2))
+
+# Extract values from dictionaries in json_response
+rows = [list(play.values()) for play in json_response]
+
+# Pulling keys of first list object
+column_names = json_response[1].keys()
+df = pd.DataFrame(rows, columns=column_names)
+
+#===============================
+# Saving file
+#===============================
+
+# Changing directory and then saving file
+os.chdir('/Users/wesjurden/Documents/GitHub/Personal/college-football-project/data/raw')
+df.to_csv('raw_draft_teams.csv', index= False)
