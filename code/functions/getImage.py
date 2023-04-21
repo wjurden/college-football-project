384cf51c3c16a571f4b26451e29eb125033879ac
wesjurden <wjurden@gmail.com>
2023-04-21 08:19:53 -0700

    Update getImage.py
    Add description of inputs to getImage function

--- code/functions/getImage.py
+++ code/functions/getImage.py
@@ -1,9 +1,14 @@
 # Defining getImage function
 def getImage(path):
+    """
+    path: full path to computer file
+    """
     # This function is created to help read an image file and returns it into a plot. 
     # It takes the argument "path" which is the full file path to the image.
     # Example: '/Users/wesjurden/Documents/GitHub/Personal/college-football-project/assets/conferences/Pac-12.png'
     
     import matplotlib.pyplot as plt
     from matplotlib.offsetbox import OffsetImage
-    return OffsetImage(plt.imread(path), zoom=.012, alpha = 1) # Adjust zoom to change size
+
+    img = OffsetImage(plt.imread(path), zoom=0.012, alpha = 1) # Adjust zoom to change size
+    return img
